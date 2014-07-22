8219c8176864934222d4339f5271f6a82d3c048c
djmattyg007 <git@matthewgamble.net>
2014-07-22 23:11:19 +1000

    Added two core HTML fixes to the schema layout.

--- protected/views/layouts/schema.php
+++ protected/views/layouts/schema.php
@@ -1,5 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 <title><?php echo Yii::app()->name; ?></title>
@@ -8,6 +7,7 @@
 <link rel="stylesheet" type="text/css" href="<?php echo BASEURL; ?>/css/main.css" />
 <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->getBaseUrl(); ?>/css/style.css" />
 
+<link rel="icon" href="<?php echo BASEURL; ?>/images/favicon.ico" type="image/x-icon" />
 <link rel="shortcut icon" href="<?php echo BASEURL; ?>/images/favicon.ico" type="image/x-icon" />
 
 <script type="text/javascript">
